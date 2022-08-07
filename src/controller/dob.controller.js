ed94106e2560d7f30724bf848e8b6a99f9d903db
Babatunde Onasanya <onasanyatunde67@gmail.com>
2022-08-07 21:28:33 +0100

    Taking care of edge case

--- src/controller/dob.controller.js
+++ src/controller/dob.controller.js
@@ -6,6 +6,9 @@ const ageController = async (req, res) => {
     const { dob } = req.query;
     const { dob: dateFromTimeStamp } = await schema.validateAsync({ dob });
     const age = getAge(dateFromTimeStamp);
+    if (age < 0) {
+      return res.status(200).json({ age: undefined });
+    }
     return res.status(200).json({ age });
   } catch (error) {
     return res.status(400).json({ error: error.message });
